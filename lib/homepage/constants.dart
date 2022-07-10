9db388e5cd673ae1dfae7f8f7f23f0a9108a6154
LordOlumide <olumideolubosede@yahoo.com>
2022-07-10 20:47:13 +0100

    creating dummy

---
+++ lib/homepage/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 17,
+  fontWeight: FontWeight.w500,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 14,
+);
+
+const kDateTimeTextStyle = TextStyle(
+  fontSize: 11,
+  color: Colors.black38,
+);
--- lib/homepage/visual_components/note_template.dart
+++ lib/homepage/visual_components/note_template.dart
@@ -1,26 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:notepad/homepage/constants.dart';
 
 class Note extends StatelessWidget {
   final String title;
+  final String body;
+  final DateTime timeLastEdited;
 
-  Note({Key? key}) : super(key: key);
+  Note({
+    Key? key,
+    required this.title,
+    required this.body,
+    required this.timeLastEdited,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 40,
       margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+      padding: const EdgeInsets.all(10),
       child: Column(
         children: [
           // Heading
-          Text(
-
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 3),
+            child: Text(
+              title != '' ? title : body,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: kTitleTextStyle,
+            ),
           ),
 
           // Body
+          title != ''
+              ? Text(
+                  body,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: kBodyTextStyle,
+                )
+              : const SizedBox(),
 
           // Date Last Edited
-
+          Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 10),
+            child: Text(
+              timeLastEdited.toString(),
+              style: kDateTimeTextStyle,
+            ),
+          )
         ],
       ),
     );
