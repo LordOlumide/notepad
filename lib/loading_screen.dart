2246b53cdd1ce5d9a74731c14cdae9569ffc2495
LordOlumide <olumideolubosede@yahoo.com>
2022-07-25 21:04:26 +0100

    Provider sucessfully served. Loading screen implemented

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -1,17 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:notepad/homepage/home_screen.dart';
+import 'package:notepad/general_components/main_database_class.dart';
+import 'package:provider/provider.dart';
+import 'package:notepad/dummy_db.dart';
+import 'general_components/note_object.dart';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  // Open the main Database
+  final mainDatabase = NotepadDatabase();
+
+  _initializeDBAndPushToHomeScreen() async {
+    // Initialize the Database
+    await mainDatabase.initializeDatabase();
+
+    // Adding the entries in the dummy db for testing purposes
+    for (int i = 0; i < dummyDatabase.length; i++) {
+      await mainDatabase.insertNote(dummyDatabase[i]);
+    }
+    // Push to home screen
+    _pushToHomeScreen();
+  }
+
+  _pushToHomeScreen() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Provider(
+          create: (context) => mainDatabase,
+          child: HomeScreen(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the main database asynchronously
+    _initializeDBAndPushToHomeScreen();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset('images/notepad.jpg'),
-
             const CircularProgressIndicator(),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:notepad/general_components/main_database_class.dart';
-import 'package:provider/provider.dart';
-import 'package:notepad/dummy_db.dart';
-import 'general_components/note_object.dart';
-
-// screens
 import 'loading_screen.dart';
-import 'package:notepad/homepage/home_screen.dart';
+
+// Note: The main Database is initialized in the Loading Screen.
+// The Provider is wrapped around the HomeScreen from the LoadingScreen.
 
 void main() {
   runApp(NotepadApp());
 }
 
-class NotepadApp extends StatefulWidget {
-  @override
-  State<NotepadApp> createState() => _NotepadAppState();
-}
-
-class _NotepadAppState extends State<NotepadApp> {
-  // Open the main Database
-  final mainDatabase = NotepadDatabase();
-
-  initializeDB() async {
-    print('before initializing DB');
-    await mainDatabase.initializeDatabase();
-
-    print('After initalizing DB');
-    // add the entries in the dummy db for testing purposes
-    for (int i = 0; i < dummyDatabase.length; i++) {
-      await mainDatabase.insertNote(dummyDatabase[i]);
-    }
-    print("After adding dummies");
-
-    // test if it's working
-    for (Note i in await mainDatabase.getNotes()) {
-      print(i.toString());
-    }
-    print('After testing');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    print('Created main DB');
-
-    // Initialize the main database asynchronously
-    initializeDB();
-
-    // Push to home screen
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => Provider(
-          create: (context) => mainDatabase,
-          child: MaterialApp(
-            home: HomeScreen(),
-          ),
-        ),
-      ),
-    );
-  }
-
+class NotepadApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const LoadingScreen();
+    return const MaterialApp(
+      home: LoadingScreen(),
+    );
   }
 }
